8879d32ccb4f0ee3595c7b47e1c19567b5964139
Morgangx <morgangbulawa@gmail.com>
2025-12-21 19:43:17 +0100

    Added player module

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main() -> None:
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen: pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock: pygame.time.Clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable= pygame.sprite.Group() 
+    Player.containers = (updatable, drawable)
+    Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt: float = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60.0)/1000
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
---
+++ player.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.key import ScancodeWrapper
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+
+class Player(CircleShape):
+    def __init__(self, x: int, y: int) -> None:
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self) -> list[pygame.Vector2]:
+        forward: pygame.Vector2 = pygame.Vector2(0, 1).rotate(self.rotation)
+        right: pygame.Vector2 = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a: pygame.Vector2 = self.position + forward * self.radius
+        b: pygame.Vector2 = self.position - forward * self.radius - right
+        c: pygame.Vector2 = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, color="white", points=self.triangle(),
+                            width= LINE_WIDTH)
+        
+    def rotate(self, dt: float) -> None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt: float) -> None:
+        keys: ScancodeWrapper = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt*-1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt*-1)
+    
+    def move(self, dt: float) -> None:
+        unit_vector: pygame.Vector2 = pygame.Vector2(0, 1)
+        rotated_vector: pygame.Vector2 = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector: pygame.Vector2 = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
